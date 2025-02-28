254fe258152cd9c8dc5f8ea0f2597d57484836f9
sminseong <star919m@gmail.com>
2025-02-28 11:32:22 +0900

    0228_01

---
+++ src/jdbc/advanced/users/DBUtil.java
@@ -0,0 +1,29 @@
+package jdbc.advanced.users;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * packageName   : jdbc.advanced.users
+ * fileName      : DBUtil
+ * author        : a
+ * date          : 2025-02-28
+ * description   : 데이터베이스 연결 작업 클래스
+ * =================================================
+ * DATE             AUTHOR             NOTE
+ * -------------------------------------------------
+ * 2025-02-28        a
+ */
+public class DBUtil {
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/ssgdb?serverTimezone=Asia/Seoul", "ssg", "ssg");
+        } catch (SQLException e) {
+            System.out.println("connect failed");
+            return null;
+        }
+    }
+
+}
